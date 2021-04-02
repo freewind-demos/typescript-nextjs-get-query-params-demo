b05ad56137cd1842e84dac838ff89c4e4f2421d6
Freewind <nowindlee@gmail.com>
2021-04-02 14:55:16 +0800

    useState

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,20 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import {useState} from 'react';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const [count, setCount] = useState(0)
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <button onClick={() => setCount(n => n + 1)}>Click to increase: {count}</button>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+    </Layout>
+  )
 
+}
 export default IndexPage
